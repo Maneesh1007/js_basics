d59ea2d83bf22d5e6ed8e1afb27cbb6094633703
Sai Maneesh <saimaneesh1115@gmail.com>
2023-08-04 13:31:57 +0530

    modifying dom

--- basic.js
+++ basic.js
@@ -1,10 +1,38 @@
-let items = document.querySelectorAll("li");
+let itemsList = document.querySelector("#items");
 
-for (let i = 0 ;i<items.length;i++){
-    if (i%2==1){
-        items[i].style.backgroundColor = "green";
-    }
-}
+console.log(itemsList.parentElement);
+
+console.log(itemsList.lastElementChild);
+
+console.log(itemsList.previousElementSibling);
+
+let newDiv = document.createElement('div');
+
+newDiv.className = "hii";
+
+newDiv.id = "hiiii";
+
+newDiv.setAttribute("tittle","hello");
+
+let insertedText = document.createTextNode("HELLo word");
+
+newDiv.appendChild(insertedText);
+
+newDiv.fontSize = "30px"
+
+let h1 = document.querySelector("#header-title");
+
+let container = document.querySelector(".container");
+
+container.insertBefore(newDiv,h1);
+
+let h2 = document.querySelector(".list-group-item");
+
+door = document.querySelector("#items");
+
+door.insertBefore(newDiv,h2);
+
+
 
 
 
