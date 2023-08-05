d6a9d3dbe789b1f095826177f2fbc1a7772c9df5
Sai Maneesh <saimaneesh1115@gmail.com>
2023-08-05 12:31:07 +0530

    scale the app

--- index.js
+++ index.js
@@ -6,17 +6,19 @@ form.addEventListener('submit',function(event)
     const name = document.getElementById("fname").value;
     const email = document.getElementById("Email").value;
     const phone = document.getElementById("phoneno").value;
-    const calltime = document.getElementById("timeforacall").value;
+  
 
     let userDetails = {
         name : name,
         email : email,
         phone : phone,
-        calltime : calltime,
+       
 
     };
     let existingUserDetailsJSON = localStorage.getItem('userDetails');
   let existingUserDetails = existingUserDetailsJSON ? JSON.parse(existingUserDetailsJSON) : [];
+
+  //console.log(userDetails["name"]+" "+userDetails["email"]+" "+userDetails["phone"]);
 
   // Add the new user object to the array
   existingUserDetails.push(userDetails);
@@ -24,4 +26,22 @@ form.addEventListener('submit',function(event)
   // Convert the updated array to JSON and store it back in local storage
   let updatedUserDetailsJSON = JSON.stringify(existingUserDetails);
   localStorage.setItem('userDetails', updatedUserDetailsJSON);
+
+  let newNode = userDetails["name"]+" --- "+userDetails["email"]+" --- "+userDetails["phone"];
+
+  
+
+  let label = document.createElement("label");
+
+  label.appendChild(document.createTextNode(newNode));
+  
+
+  form.appendChild(label);
+
+  const nameBreak = document.createElement("br");
+  form.appendChild(nameBreak);
+
+  
+
 })
+
